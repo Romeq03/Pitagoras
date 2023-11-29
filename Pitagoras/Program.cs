1a95f32e95b47178149e2f67eb0478ed1ef1c2cd
Romeq03 <romekbartek18@gmail.com>
2023-11-29 01:30:13 +0100

    Dodaj pliki projektów.

---
+++ Pitagoras/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    //zad1
+    public static double Pitagoras(double a, double b)
+    {
+        double przeciwprostokatna = a * a + b * b;
+        return przeciwprostokatna;
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Math.Sqrt(Pitagoras(3.5, 6)));
+    }
+}
